09023f0e5e65365b84383e5556c8094c43441a49
pekieend <daymeijroos@gmail.com>
2022-12-08 21:20:32 +0100

    Initial commit

---
+++ src/main/java/com/daymeijroos/iacspring/IacSpringApplication.java
@@ -0,0 +1,13 @@
+package com.daymeijroos.iacspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IacSpringApplication.class, args);
+	}
+
+}
