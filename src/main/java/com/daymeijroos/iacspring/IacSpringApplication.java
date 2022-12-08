50799da5d1d412d4312bd149ba0ada34bff3e721
pekieend <daymeijroos@gmail.com>
2022-12-08 22:01:36 +0100

    Added product model

--- src/main/java/com/daymeijroos/iacspring/IacSpringApplication.java
+++ src/main/java/com/daymeijroos/iacspring/IacSpringApplication.java
@@ -2,12 +2,15 @@ package com.daymeijroos.iacspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class IacSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IacSpringApplication.class, args);
 	}
+
 
 }
---
+++ src/main/java/com/daymeijroos/iacspring/model/Product.java
@@ -0,0 +1,38 @@
+package com.daymeijroos.iacspring.model;
+
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table
+public class Product {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String id;
+
+    @Column
+    @NotNull(message="Name cannot be empty.")
+    private String name;
+
+    @Column
+    @NotNull(message="Description cannot be empty.")
+    private String description;
+
+    @Column
+    @NotNull(message="Price cannot be empty.")
+    private Float price;
+
+    @Column
+    @NotNull(message="Image cannot be empty.")
+    private String imageURL;
+}
